d11742100f8f642f0a9fb1884efb42d33b8e1839
ThiagoThe <nitrox2thi@gmail.com>
2023-05-19 22:30:45 -0300

    "refactor: criado switch"

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const fs = require("fs");
 
 const server = http.createServer((request, response) => {
   if (request.url === "/") {
-    fs.readFile("index.html", (err, data) => {
-      if (err) {
-        response.writeHead(404);
-        response.write("Not Found!");
-      } else {
+    switch (request.method) {
+      case "GET":
         response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(data);
-      }
-      response.end();
-    });
-  } else {
-    response.writeHead(404);
-    response.write("Not Found!");
-    response.end();
+        fs.createReadStream("./index.html", "UTF-8").pipe(response);
+        break;
+      case "POST":
+        let body = "";
+        request.on("data", (chunk) => {
+          body += chunk;
+        });
+        request.on("end", () => {});
+        break;
+    }
   }
 });
